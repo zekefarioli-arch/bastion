a9f25a8873cfd50a758f9d058a85fb595dc46ed6
Ezequiel Farioli <zekefarioli@gmail.com>
2026-08-18 22:22:43 +0100

    feat: add GET /tickets endpoint

--- src/main/java/com/zekefarioli/bastion/controller/TicketController.java
+++ src/main/java/com/zekefarioli/bastion/controller/TicketController.java
@@ -1,7 +1,13 @@
 package com.zekefarioli.bastion.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zekefarioli.bastion.model.Ticket;
 import com.zekefarioli.bastion.service.TicketService;
+
 
 @RestController
 public class TicketController {
@@ -10,4 +16,10 @@ public class TicketController {
     public TicketController(TicketService ticketService) {
         this.ticketService = ticketService;
     }
+
+    @GetMapping("/tickets")
+    public List<Ticket> getAllTickets() {
+        return ticketService.findAll();
+    }
+    
 }
